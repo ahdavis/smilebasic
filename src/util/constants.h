192cb2a6efc3705d649ffe49f438fe0d130f028c
ahdavis <ahdavis13@gmail.com>
2020-03-19 15:36:42 -0500

    Added constants file

---
+++ src/util/constants.h
@@ -0,0 +1,41 @@
+/*
+ * constants.h 
+ * Declares constants for SmileBASIC
+ * Created on 3/19/2020
+ * Created by Andrew Davis
+ *
+ * Copyright (C) 2020  Andrew Davis
+ *
+ * This program is free software: you can redistribute it and/or modify   
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//include
+#include <string>
+
+/**
+ * Contains constants
+ */
+namespace Constants {
+	/**
+	 * The name of the interpreter
+	 */
+	extern const std::string PROG_NAME;
+
+	/**
+	 * The current version of the interpreter
+	 */
+	extern const std::string VERSION;
+}
+
+//end of header
